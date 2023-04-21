d5670c974b4cb9fec5ee899111ba5392998feeda
2G <80833760+YoHRa2G@users.noreply.github.com>
2023-04-21 22:26:21 +0200

    Update main.py

--- Flussometro 3.0/main.py
+++ Flussometro 3.0/main.py
@@ -3,34 +3,40 @@ import pygame
 import time
 import random
 import threading
+from tkinter import *
 
 def clockwork():
     while True:
-        global my_var
-        if my_var == False:
-            my_var=True
+        global var_timer
+        if var_timer == False:
+            var_timer=True
         time.sleep(60)
 
 
 
 # Carica il classificatore pre-addestrato
+root = Tk()
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 faceslung=0
 running=True
 personedent=0
 pygame.init()
 magenta = (230,0,126)
-giallo= (255,255,255)
-alt = 1080
-larg= 1920
+bianco= (255,255,255)
+alt = root.winfo_screenheight()
+print(alt)
+larg= root.winfo_screenwidth()
+print(larg)
 numbackgr = 1
+faceslungex= 0
 random_number=1
 background = pygame.image.load("background/"+str(numbackgr)+".png")
-screen = pygame.display.set_mode((larg,alt), pygame.FULLSCREEN)#, pygame.FULLSCREEN
+background = pygame.transform.scale(background, (larg, alt))
+screen = pygame.display.set_mode((larg,alt), pygame.FULLSCREEN)
 myFont = pygame.font.SysFont("beba.ttf", 600)
 pygame.mouse.set_visible(False)
 screen.blit(background, (0, 0))
-labelDisplay = myFont.render(str(personedent),1, giallo)
+labelDisplay = myFont.render(str(personedent),1, bianco)
 labelDisplayC= labelDisplay.get_rect(center=(larg // 2, alt // 2))
 screen.blit(labelDisplay, labelDisplayC)
 fade_surface = pygame.Surface((larg, alt))
@@ -39,7 +45,7 @@ pygame.display.update()
 
 # Accedi alla webcam
 cap = cv2.VideoCapture(0)
-my_var=False
+var_timer=False
 t = threading.Thread(target=clockwork, args=())
 t.start()
 
@@ -47,37 +53,36 @@ while running== True:
     
     # Leggi un frame dalla webcam
     ret, frame = cap.read()
-    current_time = time.localtime()
     # Converti l'immagine in scala di grigi
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Rileva le facce nell'immagine
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.01, minNeighbors=5)
-    faceslungex = faceslung -1 
-    # Stampa il numero di facce rilevate
-    if (len(faces)==faceslung):
-        pass
-    else:
-        faceslung= len(faces)
-    if (faceslung== faceslungex):
-        personedent = personedent + 1
+    faceslungex = faceslung #Variabile di controllo per vedere quante persone c'erano prima del nuovo loop
+    #Conta quante facce sono rilevate dalla cam
+    faceslung= len(faces)
+    num_sott= (faceslungex)-faceslung
+    #Incremento del contatore quando qualcuno esce dall'inquadratura
+    if (faceslung<faceslungex):
+        personedent = personedent + num_sott
         screen.fill(magenta)
         screen.blit(background, (0, 0))
 
-        if my_var == True:
-            my_var=False
+        if var_timer == True:
+            var_timer=False
             random_number = random.randint(1, 5)
             numbackgr= random_number
             background = pygame.image.load("background/"+str(numbackgr)+".png")
+            background = pygame.transform.scale(background, (larg, alt))
 
             for alpha in range(0, 37):
                 background.set_alpha((alpha*8)+4)
                 screen.blit(background, (0, 0))
-                labelDisplay = myFont.render(str(personedent),1, giallo)
+                labelDisplay = myFont.render(str(personedent),1, bianco)
                 labelDisplayC= labelDisplay.get_rect(center=(larg // 2, alt // 2))
                 screen.blit(labelDisplay, labelDisplayC)
                 pygame.display.update()
 
-        labelDisplay = myFont.render(str(personedent),1, giallo)
+        labelDisplay = myFont.render(str(personedent),1, bianco)
         labelDisplayC= labelDisplay.get_rect(center=(larg // 2, alt // 2))
         screen.blit(labelDisplay, labelDisplayC)
         pygame.display.update()
